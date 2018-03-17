fb2138b44e8511bbad393892f3b3cbb70519c41e
Matt Bretl <mpbretl@gmail.com>
2018-03-17 17:02:35 +0000

    Support deriving values from multiple columns

--- src/DerivedFieldPlugin.js
+++ src/DerivedFieldPlugin.js
@@ -7,7 +7,6 @@ function DerivedFieldPlugin(
       extend,
       pgIntrospectionResultsByKind: introspectionResultsByKind,
       graphql: { GraphQLString },
-      pgColumnFilter,
       fieldDataGeneratorsByFieldNameByType,
     } = build;
     const {
@@ -35,50 +34,80 @@ function DerivedFieldPlugin(
       throw new Error("Could not determine the type for this table");
     }
 
-    const derivedFields = introspectionResultsByKind.attribute
-      .filter(attr => attr.classId === table.id)
-      .filter(attr => pgColumnFilter(attr, build, context))
-      .reduce((memo, attr) => {
-        for (let def of derivedFieldDefinitions) {
-          if (
-            def.identifiers.includes(
-              `${table.namespaceName}.${table.name}.${attr.name}`
-            )
-          ) {
-            const fieldName = inflection.column(
-              attr.name,
-              table.name,
-              table.namespaceName
-            );
-            const derivedFieldName = def.inflect(fieldName);
-            if (memo[derivedFieldName]) {
-              throw new Error(
-                `Derived field '${derivedFieldName}' conflicts with existing GraphQL field`
-              );
-            }
-            const field = fields[fieldName];
-            const { resolve: oldResolve } = field;
-            memo[derivedFieldName] = fieldWithHooks(
-              derivedFieldName,
-              ({ addDataGenerator }) => {
-                const generatorsByField = fieldDataGeneratorsByFieldNameByType.get(
-                  Self
-                );
-                generatorsByField[fieldName].forEach(g => {
-                  addDataGenerator(g);
-                });
-                return {
-                  type: def.returnType || GraphQLString,
-                  resolve: (...resolveParams) => {
-                    const val = oldResolve(...resolveParams);
-                    return def.resolve(val);
-                  },
-                };
-              },
-              { pgFieldIntrospection: attr }
-            );
+    const derivedFields = derivedFieldDefinitions
+      .map(def => {
+        // Map string identifiers to { table, columns }
+        const identifiers = def.identifiers.map(ident => {
+          if (typeof ident !== "string") {
+            return ident;
           }
+          const t = ident.substring(0, ident.lastIndexOf("."));
+          const c = ident.substring(ident.lastIndexOf(".") + 1, ident.length);
+          return {
+            table: t,
+            columns: [c],
+          };
+        });
+        return Object.assign({}, def, { identifiers });
+      })
+      .reduce((memo, def) => {
+        const columns = def.identifiers
+          .filter(
+            ident => ident.table === `${table.namespaceName}.${table.name}`
+          )
+          .reduce((memo, ident) => ident.columns, []);
+        if (columns.length === 0) {
+          // No matches for this definition
+          return memo;
         }
+        // Generate the derived field
+        const attrs = introspectionResultsByKind.attribute.filter(
+          attr => attr.classId === table.id && columns.includes(attr.name)
+        );
+        const fieldNames = attrs.map(attr =>
+          inflection.column(attr.name, table.name, table.namespaceName)
+        );
+        const derivedFieldName = def.inflect(...fieldNames);
+        if (memo[derivedFieldName]) {
+          throw new Error(
+            `Derived field '${derivedFieldName}' conflicts with existing GraphQL field`
+          );
+        }
+        memo[derivedFieldName] = fieldWithHooks(
+          derivedFieldName,
+          ({ addDataGenerator }) => {
+            const generatorsByField = fieldDataGeneratorsByFieldNameByType.get(
+              Self
+            );
+            const setAlias = (fn, fieldName) => {
+              return (...args) => {
+                args[0].alias = fieldName;
+                return fn(...args);
+              };
+            };
+            for (let fieldName of fieldNames) {
+              for (let g of generatorsByField[fieldName]) {
+                addDataGenerator(setAlias(g, fieldName));
+              }
+            }
+            return {
+              type: def.returnType || GraphQLString,
+              description: def.description,
+              resolve: data => {
+                if (
+                  fieldNames.filter(n => !data.hasOwnProperty(n)).length > 0
+                ) {
+                  throw new Error(
+                    `Derived field '${derivedFieldName}' could not be resolved`
+                  );
+                }
+                const args = fieldNames.map(fieldName => data[fieldName]);
+                return def.resolve(...args);
+              },
+            };
+          },
+          {}
+        );
         return memo;
       }, {});
 
